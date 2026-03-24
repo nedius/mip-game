2bdd24ded5ce6a790e53283e5ae0932813749765
Simfonija12 <122538957+Simfonija12@users.noreply.github.com>
2026-03-24 21:39:03 +0200

    Refactoring
    fix error
    improve code logic

--- game_window.py
+++ game_window.py
@@ -5,13 +5,19 @@ class GameWindow(ctk.CTkToplevel):
 
     def __init__(self, master):
         super().__init__(master)
+        self.game_window_setup()
+        self.game_window_widgets()
+
+    def game_window_setup(self):
 
         self.title("My new window")
-        self.geometry("300x300")
+        self.geometry("300x300")  
 
-        def on_close(self):
-            self.master.deiconify()
-            self.destroy()      
+    def on_close(self):
+        self.master.deiconify()
+        self.destroy() 
+
+    def game_window_widgets(self):     
 
         self.new_button=ctk.CTkButton(
                                     self, 
--- start_menu_window.py
+++ start_menu_window.py
@@ -5,6 +5,13 @@ from game_window import GameWindow
 
 class StartMenuWindow(ctk.CTk):
 
+    def __init__(self):
+        super().__init__()
+        self.setup_config()
+        self.create_widgets()
+    
+    #Function that will use GUI
+
     def on_diapason_entry(self, event):
         diapason = self.diapason_entry.get()
         # Validate the input and start the game with the given diapason
@@ -14,26 +21,45 @@ class StartMenuWindow(ctk.CTk):
         pass
     
     def update_entry(self, value):
-        self.silder_var.set(str(int(float(value)))) # Update the entry field with the slider value, converting it to an integer and then to a string
+        self.slider_var.set(str(int(float(value)))) # Update the entry field with the slider value, converting it to an integer and then to a string
 
     def validate_entry(self, event):
-        value = self.diapason_entry.get()
-        if value.isdigit() and 15 <= int(value) <= 25:
-            self.silder_var.set(int(value))  # Update the slider with the entry value
-        else:
-            self.diapason_entry.delete(0, ctk.END)  # Clear the entry if it's invalid
-            self.diapason_entry.insert(0, str(self.silder_var.get()))  # Reset to the current slider value    
+        check_value = self.diapason_entry.get()
 
-    def __init__(self):
-        super().__init__()
+        if not check_value:
+            return
+        
+        try:
+            num_value=int(check_value)
+            if 15<=num_value<=25:
+                self.slider_var.set(num_value)
+            else:
+                self.diapason_entry.delete(0, ctk.END)
+                self.diapason_entry.insert(0, str(self.slider_var.get()))
 
+        except ValueError:
+            self.diapason_entry.delete(0, ctk.END)
+            self.diapason_entry.insert(0, str(self.slider_var.get()))
+
+    def start(self):
+
+        self.withdraw()
+
+        self.game_view=GameWindow(master=self)
+        self.game_view.lift()#in first layer      
+
+    #Game window setup
+
+    def setup_config(self):
+        
         self.title("mip-game")
         self.geometry("300x300")
         self.resizable(False, False)  #Disable window resizing
+    
+        self.slider_var = ctk.IntVar(value="20")
+        self.check_var=ctk.StringVar(value="off")
 
-        
-
-        self.silder_var = ctk.IntVar(value=20)
+    def create_widgets(self):
 
         self.logo=ctk.CTkImage(Image.open("game-control.png"), size=(40, 40))
         self.logo_label = ctk.CTkLabel(master=self, image=self.logo, text="")
@@ -45,19 +71,21 @@ class StartMenuWindow(ctk.CTk):
         self.diapason_label = ctk.CTkLabel(self, text="Enter a number from 15 to 25:")
         self.diapason_label.grid(row=3, column=0, padx=(20, 20), sticky="nsew")
 
-        self.diapason_entry = ctk.CTkEntry(self, textvariable=self.silder_var, width=60, justify="center")
+        self.diapason_entry = ctk.CTkEntry(self, width=60, justify="center")
         self.diapason_entry.grid(row=4, column=0, padx=20, pady=10, sticky="ns")
+        self.diapason_entry.insert(0, "20")
 
-        # self.silder_var=ctk.IntVar(value=20)
+        # self.slider_var=ctk.IntVar(value=20)
         self.slider = ctk.CTkSlider(self, 
                                     from_=15, 
                                     to=25,
                                     number_of_steps=10, #How many discrete steps the slider should have
-                                    variable=self.silder_var,
+                                    variable=self.slider_var,
                                     height=20,)
         self.slider.grid(row=5, column=0, padx=80, pady=5, sticky="ew")
 
-        self.check_var=ctk.StringVar(value="off")
+        self.diapason_entry.bind("<Return>", self.validate_entry)  # Bind the Enter key to validate the entry
+
         self.check_box=ctk.CTkCheckBox(
                                 master=self,
                                 text="Start first",
@@ -70,15 +98,8 @@ class StartMenuWindow(ctk.CTk):
 
         self.grid_columnconfigure(0, weight=1)  # Make the column expand to fill the window
 
-        self.diapason_entry.bind("<Return>", self.validate_entry)  # Bind the Enter key to validate the entry
+
         
-    def start(self):
-
-        self.withdraw()
-
-        self.game_view=GameWindow(master=self)
-        self.game_view.lift()#in first layer
-
 
 if __name__ == "__main__":
     app = StartMenuWindow()
